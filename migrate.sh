5070c816d2e752edaaf703712a16824843f4e43d
Diego <diego.antoniosantamaria@gmail.com>
2025-06-23 14:18:58 +0200

    tweaks to migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -3,18 +3,18 @@
 source .env
 
 if [ $1 = "--fresh" ]; then
-	mysql -h$DB_HOST -u$DB_USERNAME -p$DB_PASSWORD -e\
-		"DROP DATABASE $DB_DATABASE;"
+    echo "DROP DATABASE $DB_DATABASE;" | mysql -h$DB_HOST -u$DB_USERNAME -p$DB_PASSWORD
 fi
 
-mysql -h$DB_HOST -u$DB_USERNAME -p$DB_PASSWORD -e\
-	"CREATE DATABASE IF NOT EXISTS $DB_DATABASE;
-	CREATE TABLE $DB_DATABASE.posts (
-		id INT NOT NULL AUTO_INCREMENT,
-		author VARCHAR(65) NOT NULL,
-		title VARCHAR(65) NOT NULL,
-		imageUrl VARCHAR(255) NOT NULL,
-		description TEXT(500) NOT NULL,
-		PRIMARY KEY(id)
-		);"
+echo "CREATE DATABASE IF NOT EXISTS $DB_DATABASE;" | mysql -h$DB_HOST -u$DB_USERNAME -p$DB_PASSWORD
+
+echo "CREATE TABLE $DB_DATABASE.posts (
+        id INT NOT NULL AUTO_INCREMENT,
+        author VARCHAR(65) NOT NULL,
+        title VARCHAR(65) NOT NULL,
+        imageUrl VARCHAR(255) NOT NULL,
+        description TEXT(500) NOT NULL,
+        PRIMARY KEY(id)
+        );
+" | mysql -h$DB_HOST -u$DB_USERNAME -p$DB_PASSWORD
 
